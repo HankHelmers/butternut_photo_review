f4cf6359d46af5a54ffad1a4b0b9cbd760ac1e9a
Hank Helmers <89431423+HankHelmers@users.noreply.github.com>
2025-10-17 11:56:39 -0400

    Feature 1b. Flask server can call custom endpoint for authentication

--- httpserver.py
+++ httpserver.py
@@ -6,7 +6,14 @@ app = flask.Flask(__name__, # Name of the main method
                   static_url_path='',
                   static_folder='public')  # host the files from the public folder
 
+# redirects user to the index.html if none is provided
+@app.route("/")
+def redirect():
+    return flask.redirect(location="/index.html")
 
+@app.get("/auth")
+def authenticate():
+    return json.dumps({"message": "authenticate called"})
 
 if __name__ == "__main__":  
     app.run(host="0.0.0.0", port=5500)
--- public/scripts/api_script.js
+++ public/scripts/api_script.js
@@ -1,87 +1,106 @@
-// import { GoogleAuth } from '/google-auth-library';
-// import { google } from '/googleapis';
-import { clientId, key } from './secrets.js'
+// // import { GoogleAuth } from '/google-auth-library';
+// // import { google } from '/googleapis';
+// import { clientId, key } from './secrets.js'
 
-// ${{ secrets.GOOGLEDRIVEAPI_CLIENTID}}
+// // ${{ secrets.GOOGLEDRIVEAPI_CLIENTID}}
 
 
-// TODO(developer): Set to client ID and API key from the Developer Console
-const CLIENT_ID = clientId;
-const API_KEY = key;
-const REDIRECT_URI = "http://localhost:5500"
+// // TODO(developer): Set to client ID and API key from the Developer Console
+// const CLIENT_ID = clientId;
+// const API_KEY = key;
+// const REDIRECT_URI = "http://localhost:5500"
 
-// reference for setting scopes: https://developers.google.com/workspace/drive/api/guides/api-specific-auth#node.js
-// scopes set in configuration: .../auth/drive.readonly	.../auth/drive.metadata
-const scopes = [
-    "https://www.googleapis.com/auth/drive.apps.readonly",
-    "https://www.googleapis.com/auth/drive.metadata"
-]; 
+// // reference for setting scopes: https://developers.google.com/workspace/drive/api/guides/api-specific-auth#node.js
+// // scopes set in configuration: .../auth/drive.readonly	.../auth/drive.metadata
+// const scopes = [
+//     "https://www.googleapis.com/auth/drive.apps.readonly",
+//     "https://www.googleapis.com/auth/drive.metadata"
+// ]; 
 
-async function authenticate() {
-    const tokenClient = google.accounts.oauth2.initTokenClient({
-        client_id: CLIENT_ID,
-        scope: scopes.join(" "),    // standard to join multiple with spaces
-        callback: (tokenResponse) => {
-            // Save token for later use
-            window.accessToken = tokenResponse.access_token;
-            console.log("Access token:", window.accessToken);
-        },
-    });
+// async function authenticate() {
+//     const tokenClient = google.accounts.oauth2.initTokenClient({
+//         client_id: CLIENT_ID,
+//         scope: scopes.join(" "),    // standard to join multiple with spaces
+//         callback: (tokenResponse) => {
+//             // Save token for later use
+//             window.accessToken = tokenResponse.access_token;
+//             console.log("Access token:", window.accessToken);
+//         },
+//     });
 
-    tokenClient.requestAccessToken();
-}
+//     tokenClient.requestAccessToken();
+// }
 
-/*
- * Create form to request access token from Google's OAuth 2.0 server.
- * Reference: https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow#authorization-errors-origin-mismatch
- */
-function oauthSignIn() {
-    // Google's OAuth 2.0 endpoint for requesting an access token
-    var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+// /*
+//  * Create form to request access token from Google's OAuth 2.0 server.
+//  * Reference: https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow#authorization-errors-origin-mismatch
+//  */
+// function oauthSignIn() {
+//     // Google's OAuth 2.0 endpoint for requesting an access token
+//     var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
 
-    // Create <form> element to submit parameters to OAuth 2.0 endpoint.
-    var form = document.createElement('form');
-    form.setAttribute('method', 'GET'); // Send as a GET request.
-    form.setAttribute('action', oauth2Endpoint);
+//     // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+//     var form = document.createElement('form');
+//     form.setAttribute('method', 'GET'); // Send as a GET request.
+//     form.setAttribute('action', oauth2Endpoint);
 
-    // Parameters to pass to OAuth 2.0 endpoint.
-    var params = {
-        'client_id': CLIENT_ID,
-        'redirect_uri': REDIRECT_URI,
-        'response_type': 'token',
-        'scope': scopes.join(" "),    // standard to join multiple with spaces
-        'include_granted_scopes': 'true',
-        'state': 'pass-through value'
-    };
+//     // Parameters to pass to OAuth 2.0 endpoint.
+//     var params = {
+//         'client_id': CLIENT_ID,
+//         'redirect_uri': REDIRECT_URI,
+//         'response_type': 'token',
+//         'scope': scopes.join(" "),    // standard to join multiple with spaces
+//         'include_granted_scopes': 'true',
+//         'state': 'pass-through value'
+//     };
 
-    // Add form parameters as hidden input values.
-    for (var p in params) {
-        var input = document.createElement('input');
-        input.setAttribute('type', 'hidden');
-        input.setAttribute('name', p);
-        input.setAttribute('value', params[p]);
-        form.appendChild(input);
+//     // Add form parameters as hidden input values.
+//     for (var p in params) {
+//         var input = document.createElement('input');
+//         input.setAttribute('type', 'hidden');
+//         input.setAttribute('name', p);
+//         input.setAttribute('value', params[p]);
+//         form.appendChild(input);
+//     }
+
+//     // Add form to page and submit it to open the OAuth 2.0 endpoint.
+//     document.body.appendChild(form);
+//     form.submit();
+// }
+
+// async function getAllFiles() {
+//     const response = await fetch("https://www.googleapis.com/drive/v3/files", {
+//         headers: {
+//             "Authorization": `Bearer ${window.accessToken}`,
+//         },
+//     });
+//     const data = await response.json();
+//     console.log(data);
+//     return data;
+// }
+
+
+async function auth() {
+    let options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/plain'
+        }
     }
 
-    // Add form to page and submit it to open the OAuth 2.0 endpoint.
-    document.body.appendChild(form);
-    form.submit();
+    try {
+        let response = await fetch("/auth", options)
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        let responseData = await response.json();
+        console.log(responseData);
+    }
+    catch (ex) {
+        console.error(ex);
+    }
 }
-
-async function getAllFiles() {
-    const response = await fetch("https://www.googleapis.com/drive/v3/files", {
-        headers: {
-            "Authorization": `Bearer ${window.accessToken}`,
-        },
-    });
-    const data = await response.json();
-    console.log(data);
-    return data;
-}
-
 
 window.addEventListener("load", (event) => {
-    // console.log(clientId);
-    // oauthSignIn();
-    // getAllFiles();
+    auth();
 });
